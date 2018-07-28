4e8098ed83e381151b13d224b2a5ceb00cf3095c
Xin Li <alexli7802@gmail.com>
2018-07-28 17:47:22 +1000

    minor check in

--- src/main/scala/example/QuickStart2.scala
+++ src/main/scala/example/QuickStart2.scala
@@ -4,6 +4,8 @@ import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl._
+import akka.stream._
+import scala.concurrent.Future
 
 final case class Author(handle: String)
 final case class Hashtag(name: String)
@@ -34,11 +36,35 @@ object QuickStart2 {
     
 	  implicit val system = ActorSystem("reactive-tweets")
 	  implicit val materializer = ActorMaterializer()
+	  
+	  val hashtags: Source[Hashtag, NotUsed] = tweets.mapConcat(t => t.hashtags.toList)
+	  
+	  val count: Flow[Tweet, Int, NotUsed] = Flow[Tweet].map(_ => 1)
+	  val sumSink: Sink[Int, scala.concurrent.Future[Int]] = Sink.fold[Int, Int](0)(_+_)
+	  val counterGraph: RunnableGraph[Future[Int]] = tweets.via(count).toMat(sumSink)(Keep.right)
+	  val sum: Future[Int] = counterGraph.run()
+	  sum.foreach(c => println(s"Total tweets processed: $c"))(system.dispatcher)
 			  
-	  tweets.map(_.hashtags)
+	  val writeAuthors: Sink[Author, NotUsed] = ???
+	  val writeHashtags: Sink[Hashtag, NotUsed] = ???
+	  
+	  val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit gb =>
+	    import GraphDSL.Implicits._
+	    
+	    val bcast = gb.add(Broadcast[Tweet](2))
+	    tweets ~> bcast.in
+	    bcast.out(0) ~> Flow[Tweet].map(_.author) ~> writeAuthors
+	    bcast.out(1) ~> Flow[Tweet].mapConcat(_.hashtags.toList) ~> writeHashtags
+	    ClosedShape	    
+	  })
+	  
+	  g.run()
+/*	  
+		tweets.map(_.hashtags)
   	  .reduce(_ ++ _)
   	  .mapConcat(identity)
   	  .map(_.name.toUpperCase)
   	  .runWith(Sink.foreach(println))
+*/  	  
   }
 }
