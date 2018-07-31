264718770391ed134b31f041441adb7cbda0952a
Alex Li <ali@cellossoftware.com>
2018-08-01 07:45:57 +1000

    modularity 2

--- src/main/scala/example/ModularityExample.scala
+++ src/main/scala/example/ModularityExample.scala
@@ -11,3 +11,39 @@ object ModularityExample {
   
   val runnableGraph = complexSource.to(complexSink)
 }
+
+object ComplexGraphExample {
+  import GraphDSL.Implicits._
+  
+  val example1 = 
+    RunnableGraph.fromGraph( GraphDSL.create() { implicit builder=>
+      val A: Outlet[Int] = builder.add(Source.single(0)).out
+      val B: UniformFanOutShape[Int,Int] = builder.add( Broadcast[Int](2) )
+      val C: UniformFanInShape[Int,Int] = builder.add( Merge[Int](2) )
+      val D: FlowShape[Int,Int] = builder.add( Flow[Int].map(_ + 1) )
+      val E: UniformFanOutShape[Int,Int] = builder.add( Balance[Int](2) )
+      val F: UniformFanInShape[Int, Int] = builder.add( Merge[Int](2) )
+      val G: Inlet[Any] = builder.add( Sink.foreach(println) ).in
+                C     <~      F
+      A ~> B ~> C     ~>      F
+           B ~> D  ~>  E  ~>  F
+                       E  ~>  G
+      ClosedShape
+    } )
+    
+  val example2 = 
+    RunnableGraph.fromGraph( GraphDSL.create() { implicit builder=>
+      val B: UniformFanOutShape[Int,Int] = builder.add( Broadcast[Int](2) )
+      val C: UniformFanInShape[Int,Int] = builder.add( Merge[Int](2) )
+      val E: UniformFanOutShape[Int,Int] = builder.add( Balance[Int](2) )
+      val F: UniformFanInShape[Int, Int] = builder.add( Merge[Int](2) )
+
+      Source.single(0) ~> B.in; B.out(0) ~> C.in(1); C.out ~> F.in(0)
+      C.in(0) <~ F.out
+      
+      B.out(1).map(_ + 1) ~> E.in; E.out(0) ~> F.in(1)
+      E.out(1) ~> Sink.foreach(println)
+
+      ClosedShape
+    } )
+}
